5e350abf2158cee8cb6b464804ee8c6e01e83e3b
Swaathi <swaathi1409@gmail.com>
2025-12-12 17:28:23 +0530

    update requirements

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\Downloads\Infosys\venv
+set VIRTUAL_ENV=C:\Users\Admin\Documents\GitHub\AI-Enabled-Visa-Status-Prediction-and-Processing-Time-Estimator\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
